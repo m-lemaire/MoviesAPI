09272cf01d21410b5778709bd0bffe5e74c8d4e4
Manon <manon.lemaire@epfedu.fr>
2022-10-25 10:58:30 +0200

    improvements

--- imbd_request/movies.py
+++ imbd_request/movies.py
@@ -5,10 +5,17 @@ from requests.models import get_auth_from_url
 class Movies:
 
     def Get_Movie_Reviews(title):
-        url = "https://imdb-api.com/en/API/"
+        url = "https://imdb-api.com/en/API"
         api_key = "k_68iqudb6"
-        
-        SearchMovie = requests.get(url+"/SearchMovie/"+api_key+"/"+title)
-        id = SearchMovie.json()["results"][0]["id"]
-        Reviews = requests.get(url+"/Reviews/"+api_key+"/"+id)
-        return Reviews
+
+        search_movie = requests.get(url+"/SearchMovie/"+api_key+"/"+title)
+        print(search_movie.json())
+        id = search_movie.json()["results"][0]["id"]
+        reviews = requests.get(url+"/Reviews/"+api_key+"/"+id)
+        return reviews
+
+
+class Movie:
+    
+    def __init__(self,title) -> None:
+        self.title = title
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from imbd_request.movies import Movies
+from imbd_request.movies import Movies, Movie
 
 
 title = "The Shawshank Redemption"
+movie = Movie(title)
+print(movie.title)
 
 Result_request = Movies.Get_Movie_Reviews(title)
 
-print(Result_request.json())
+# print(Result_request.json())
 
